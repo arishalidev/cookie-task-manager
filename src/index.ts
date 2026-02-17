75d7e30d323146ca85b28584539c17862889db70
aalilabs <hello@arishali.dev>
2026-02-17 08:50:35 -0600

    started new oven button

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log('Happy developing ✨')
+function createNewOven() {
+    window.location.href = "./oven.html"
+}
+
+const newOvenButton = document.getElementById("newOven");
+newOvenButton?.addEventListener("click", createNewOven);
