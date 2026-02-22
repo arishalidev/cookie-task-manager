9133b7cef9cd3cfdf85be8b8cc602e0d0814f6af
aalilabs <hello@arishali.dev>
2026-02-22 11:36:40 -0600

    Added modal popover for new oven button

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,1 @@
-function createNewOven() {
-    window.location.href = "./oven.html"
-}
-
 const newOvenButton = document.getElementById("newOven");
-newOvenButton?.addEventListener("click", createNewOven);
