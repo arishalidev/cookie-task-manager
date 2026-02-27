f9d1829ad960b3325e63e1e8ddeb3de1898ff574
aalilabs <hello@arishali.dev>
2026-02-27 08:19:00 -0600

    Added local storage to new ovens

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,43 @@
 const newOvenForm = document.querySelector<HTMLFormElement>('#newOvenForm');
 
+interface CookieData {
+    description: string;
+}
+
+interface OvenData {
+    title: string;
+    description: string;
+    tag: number;
+    priority: number;
+    cookies: CookieData[];
+
+}
+
 newOvenForm?.addEventListener('submit', (event: SubmitEvent) => {
     event.preventDefault();
 
     const formData = new FormData(newOvenForm);
-    const data = Object.fromEntries(formData.entries());
 
-    console.log('Form Data:', data);
+    const ovenTitle: string = formData.get('title') as string ?? "";
+    const ovenDescription: string = formData.get('description') as string ?? "";
+    const ovenCookies: string[] = formData.getAll('cookies') as string[] ?? [];
+
+    const cookies: CookieData[] = [];
+    for (let i: number = 0; i < ovenCookies.length; i++) {
+        const cookie: CookieData = {
+            description: ovenCookies[i]
+        }
+
+        cookies.push(cookie);
+    }
+
+    const ovenData: OvenData = {
+        title: ovenTitle,
+        description: ovenDescription,
+        tag: 0,
+        priority: 0,
+        cookies: cookies
+    };
+
+    localStorage.setItem('oven_data', JSON.stringify(ovenData));
 });
