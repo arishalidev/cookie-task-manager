6ee54a87986d4b89ffd118fced379173850723a3
aalilabs <hello@arishali.dev>
2026-02-27 07:45:09 -0600

    Added event listener for creating new oven, Updated ts version

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-const newOvenButton = document.getElementById("newOven");
+const newOvenForm = document.querySelector<HTMLFormElement>('#newOvenForm');
+
+newOvenForm?.addEventListener('submit', (event: SubmitEvent) => {
+    event.preventDefault();
+
+    const formData = new FormData(newOvenForm);
+    const data = Object.fromEntries(formData.entries());
+
+    console.log('Form Data:', data);
+});
